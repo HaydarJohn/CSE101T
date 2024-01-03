d2cd4b09f5ae85095c307455725ca08238eb9cf8
haydarjohn <haydarcan_2003@hotmail.com>
2024-01-03 13:18:43 +0300

    update

--- Assingment04/Assignment4_20220808052.java
+++ Assingment04/Assignment4_20220808052.java
@@ -1,8 +1,20 @@
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Assignment4_20220808052 
 {
+    public static void main(String[] args)
+    {
+        //hocalara soruyorlar ne yapacaklarini
+        //tum dunyadan yayinlar bakip gelismeleri arastiriyorlar
+        int count = countCategory("a.txt");
+        String[] a = new String[count];
+        int[] b = new int[count];
+        int[] c = new int[count];
+        
+        getCategory(a, b, c,"a.txt");
+    }
 
 
     public static int countCategory(String filename)
@@ -36,10 +48,38 @@ public class Assignment4_20220808052
                 category[i] = formatCategoryName(scan.next());
                 quantity[i] = Integer.parseInt(scan.next());
                 weight[i] = Integer.parseInt(scan.next());
+                System.out.println(category[i]);
+                System.out.println(quantity[i]);
+                System.out.println(weight[i]);
             }
 
         }
         catch(Exception e)
+        {
+            // TODO:Error Handling
+        }
+
+    }
+
+    public static void writeGrades(String[] student,double[] grade,String studentGrades,String errorLog)
+    {
+        File strudentFile = new File(studentGrades);
+        File errorFile = new File(errorLog);
+        try
+        {
+            PrintWriter errorWriter = new PrintWriter(errorFile);
+            PrintWriter studentWriter = new PrintWriter(strudentFile);
+            
+
+
+            //temporary
+            for(int i=0;i<student.length;i++)
+            {
+                studentWriter.print(student[i]);
+                studentWriter.print(grade[i]);
+            }
+        }
+        catch (Exception e)
         {
             // TODO:Error Handling
         }
